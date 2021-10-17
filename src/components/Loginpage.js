c99616264538468ae3515bac33f4ad32f2940756
delphine nyaboke <delphine@sendyit.com>
2021-10-17 13:52:04 +0300

    login-page

---
+++ src/components/Loginpage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../index.css';
+// 
+const Loginpage =() => {
+        return (
+        <div>
+            <form>
+
+                {/* <div className="sign-in"> 
+                <h3>Sign In</h3>
+                </div> */}
+            
+                <div className="form-group">
+                    <h3>Sign In</h3>
+
+                    <input type="email" className="form-control" placeholder=" Enter Email" /><br/>
+                
+                    <input type="password" className="form-control" placeholder=" Enter Password" /><br/>
+
+                    <div>
+                        <input type="checkbox" id="terms" name="terms" />
+                        <label for="terms">Remember Me</label> <br/>
+                    </div>
+
+                    <button type="submit" className="btn">Log In</button>
+                    <p className="forgot-password text-right">
+                        Forgot My <a href="#">Password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+                 );
+}
+
+export default Loginpage;
