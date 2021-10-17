709cf17545ebdc9194f8ba42d70641d13c2ddcc2
delphine nyaboke <delphine@sendyit.com>
2021-10-17 11:35:22 +0300

    designed signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import './App.css';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-       <p>yakitabu.io </p>    
-       <p>Hello World!</p>           
+       <SignUp/>          
     </div>
   );
 }
 
 export default App;
+
+
