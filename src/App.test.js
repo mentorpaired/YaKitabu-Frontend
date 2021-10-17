fde5f40a49e40f51236eadad06ea80c4cf51e9ed
delphine nyaboke <delphine@sendyit.com>
2021-10-17 22:36:36 +0300

    this?

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page link', () => {
   render(<App />);
 
   const accountElement = screen.getByText(/I have an account/i);
