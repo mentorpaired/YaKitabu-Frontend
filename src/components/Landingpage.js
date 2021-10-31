f191513187029cfbe9468a148140febb658ed111
Brenii <nyakoabrenda@gmail.com>
2021-10-31 10:56:09 +0300

    resolved comments by Arinze

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -2,7 +2,7 @@
 function Landingpage() {
   return (
     <div>
-      <div className="btns">
+      <div className="wrapper">
         <button className="btn">Sign Up</button>
         <h4>I have an account</h4>
         <button className="btn">Sign In</button>
